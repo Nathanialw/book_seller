e374ed789f5b2d4f7fca23acb66da73e100ef3cc
nathanial <nathanial28@gmail.com>
2025-08-11 06:10:04 -0600

    updated pkg

--- cmd/main.go
+++ cmd/main.go
@@ -25,3 +25,18 @@ func main() {
 	log.Println("Starting server on :6600")
 	http.ListenAndServe(":6600", r)
 }
+
+func Run() {
+	gob.Register([]models.CartItem{})
+
+	db.InitDB()
+	if err := cache.LoadCache(); err != nil {
+		log.Fatalf("Failed to load genres: %v", err)
+	}
+
+	r := routes.SetupRoutes()
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("Starting server on :6600")
+	http.ListenAndServe(":6600", r)
+}
